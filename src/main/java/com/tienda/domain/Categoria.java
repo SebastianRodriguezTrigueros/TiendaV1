566c6306905a959cfc922d0dad802cd45d6961c8
SebastianRodriguezTrigueros <sebasrt@gmail.com>
2024-07-01 20:59:08 -0600

    Clase6

---
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -0,0 +1,30 @@
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data //especificar que es una capa de datos
+@Entity //esta clase esta enlazada a una tabla en la BD
+@Table(name = "categoria") //especificar cual entidad
+
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L; //autoincrement 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+
+    private Long idCategoria;
+    private String descripcion;
+    private String rutaImagen;
+    private boolean activo;
+
+    public Categoria() {
+    }
+    public Categoria(String categoria, boolean activo) {
+        this.descripcion = categoria;
+        this.activo = activo;
+    }
+}
